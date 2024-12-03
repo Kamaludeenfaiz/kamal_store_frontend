2b65cf901186c165c3a7a6efdfaa2766d417deb9
Kamaludeenfaiz <kamaluddeenfaiz1@gmail.com>
2024-12-03 15:26:54 +0100

    Added the header component

---
+++ src/components/HeaderCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <i class="bi bi-marker-tip" style="color: green"></i>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <ul class="navbar-nav ms-auto">
+        <button
+          class="btn btn-light"
+          :disabled="cartCount === 0"
+          @click="toggleCart"
+        >
+          <i class="bi bi-cart3"></i>
+          <span class="text-success fw-bolder">({{ cartCount }})</span>
+        </button>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['cartCount'], // Number of items in the cart
+  methods: {
+    toggleCart() {
+      this.$emit('toggle-cart')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.navbar {
+  padding: 10px 0;
+  margin-bottom: 4rem;
+}
+
+.navbar .nav-item {
+  padding-left: 5px;
+  font-size: 16px;
+}
+
+.navbar a {
+  text-decoration: none;
+  color: black !important;
+}
+
+.navbar .bi {
+  font-size: 24px;
+  font-weight: bolder;
+}
+</style>
